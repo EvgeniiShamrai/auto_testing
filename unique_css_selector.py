3b62eb4992b7c87648c678656363bbf4a23e7244
Evgeny Shamray <EvSha83@yandex.ru>
2022-08-11 19:45:41 +0300

    fix simple  test unique css selector

--- unique_css_selector.py
+++ unique_css_selector.py
@@ -8,10 +8,11 @@ from selenium.webdriver.common.by import By
 class UniqueSelector(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
-        self.urls = ["http://suninjuly.github.io/registration1.html","http://suninjuly.github.io/registration2.html"]
+
+
 
     def test_find_unique_selector_by_first_url(self):
-        self.driver.get(self.urls[0])
+        self.driver.get("http://suninjuly.github.io/registration1.html")
         sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your first name']").send_keys("Jonh")
         sleep(1)
@@ -20,12 +21,14 @@ class UniqueSelector(unittest.TestCase):
         self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your email']").send_keys("www@mail.ru")
         sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "button.btn").click()
+        sleep(1)
         final_text = self.driver.find_element(By.TAG_NAME, "h1").text
         expected_text = "Congratulations! You have successfully registered!"
         self.assertEqual(expected_text, final_text, "The text does not match the expected result")
+        self.driver.quit()
 
     def test_find_unique_selector_by_second_url(self):
-        self.driver.get(self.urls[1])
+        self.driver.get("http://suninjuly.github.io/registration2.html")
         sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your first name']").send_keys("Jonh")
         sleep(1)
@@ -34,9 +37,12 @@ class UniqueSelector(unittest.TestCase):
         self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your email']").send_keys("www@mail.ru")
         sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "button.btn").click()
+        sleep(1)
         final_text = self.driver.find_element(By.TAG_NAME, "h1").text
         expected_text = "Congratulations! You have successfully registered!"
         self.assertEqual(expected_text, final_text, "The text does not match the expected result")
+        self.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
